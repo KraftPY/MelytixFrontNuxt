5e76838688420442ff071ef5a74dd7962c132cb3
Kraft Pavel <48793976+KraftPY@users.noreply.github.com>
2021-01-30 18:20:48 +0200

    first push

---
+++ pages/index.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="container">
+    <h1 class="test">
+      Hello
+    </h1>
+    <div>
+      <Logo />
+      <h1 class="title">
+        Melytix
+      </h1>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--green"
+        >
+          Documentation
+        </a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--grey"
+        >
+          GitHub
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  display: block;
+  letter-spacing: 1px;
+  color: $secondary;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: $secondary;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
